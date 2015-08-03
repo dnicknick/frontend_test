1a6f5a08e2e5f2837c4d641708f49a56cb8d6382
dnn <dniknik@google.com>
2015-08-03 06:20:47 +0300

    commit fixes

---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+var gulp = require('gulp');
+var concatCss = require('gulp-concat-css');
+var rename = require("gulp-rename");
+var minifyCss = require('gulp-minify-css');
+var uncss = require('gulp-uncss');
+var notify = require("gulp-notify");
+var ngmin = require('gulp-ngmin');
+var htmlify = require('gulp-angular-htmlify');
+var debug = require('gulp-debug');
+
+//var browserSync = require('browser-sync').create();
+//return gulp.src(['src/partials/**/*.html', 'src/scripts/**/*.js'])
+
+gulp.task('htmlify', function() {
+  gulp.src('src/**/*.html')
+    //.pipe(debug({title: 'unicorn:'}))
+    .pipe(htmlify())
+    //.pipe(debug({title: 'unicorn:'}))
+    .pipe(gulp.dest('app/'))
+    //.pipe(notify('Gulp build:html done!'))
+    ;
+});
+
+gulp.task('ngmin', function () {
+  return gulp.src('src/js/app.js')
+    .pipe(ngmin({dynamic: true}))
+    .pipe(gulp.dest('app/js'))
+    //.pipe(notify('Gulp build:js done!'))
+    ;
+});
+
+//gulp.task('minify-css', function() {
+//  return gulp.src('src/style/*.css')
+//    .pipe(minifyCss({compatibility: 'ie8'}))
+//    .pipe(gulp.dest('dist'));
+//});
+
+gulp.task('clean', function () {
+  //rimraf(path.clean, cb);
+  
+});
+
+gulp.task('css', function () {
+  return gulp.src('src/style/*.css')
+    //.pipe(debug({title: 'unicorn:'}))
+    .pipe(concatCss("style/style.css"))
+    //.pipe(uncss({
+    //  html: ['index.html', 'pages/**/*.html', 'http://example.com']
+    //}))
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(rename("style/style.min.css"))
+    .pipe(gulp.dest('app/'))
+    //.pipe(notify('Gulp build:css done!'))
+    ;
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/style/*.css', ['css'])
+  gulp.watch('src/js/*.js' , ['ngmin'])
+  gulp.watch('src/**/*.html' , ['htmlify'])
+});
+
+gulp.task('build', ['css', 'ngmin', 'htmlify']);
+
+gulp.task('default',  ['clean', 'css', 'ngmin', 'htmlify']);
+
+// Static server
+//gulp.task('browser-sync', function() {
+//  browserSync.init({
+//    server: {
+//      baseDir: "./"
+//    }
+//  });
+//});
+
+// or...
+//gulp.task('browser-sync', function() {
+//  browserSync.init({
+//    proxy: "yourlocal.dev"
+//  });
+//});
+
+
+//var gulp = require('gulp');
+//var cssmin = require('gulp-cssmin');
+//var rename = require('gulp-rename');
+//
+//gulp.task('default', function () {
+//  gulp.src('src/**/*.css')
+//    .pipe(cssmin())
+//    .pipe(rename({suffix: '.min'}))
+//    .pipe(gulp.dest('dist'));
+//});
---
+++ src/js/app.js
@@ -0,0 +1,150 @@
+function send_on_server() {
+  document.forms.form.submit(); //@fixme
+}
+
+var angApp = angular.module('angApp', ['ngRoute']);
+
+angApp.config(function($routeProvider) {
+  $routeProvider
+
+    .when('/', {
+      templateUrl: 'pages/home.html',
+      controller: 'mainController'
+    })
+
+    //.when('/about', {
+    //  templateUrl: 'pages/about.html',
+    //  controller: 'aboutController'
+    //})
+    //
+    //.when('/contact', {
+    //  templateUrl: 'pages/contact.html',
+    //  controller: 'contactController'
+    //})
+
+    .when('/items', {
+      templateUrl: 'pages/items.html',
+      controller: 'itemsController'
+    });
+});
+
+angApp.controller('mainController', function ($scope) {
+  console.log("mainController");
+});
+
+angApp.controller('aboutController', function ($scope) {
+  console.log("aboutController");
+});
+
+angApp.controller('contactController', function ($scope) {
+  console.log("contactController");
+});
+
+angApp.controller('itemsController', function ($scope) {
+  console.log("itemsController");
+  //$scope.incomplete = true;
+
+  $scope.build_items = function () {
+    console.log("build_items = function() {");
+    //@fixme validate
+
+    var count_all_ratings = $scope.model.ratesCount;
+
+    $scope.model.items_rate = [];
+    if (!count_all_ratings || count_all_ratings <= 0) {
+      return;
+    }
+
+    $scope.model.items_rate = $scope._createRateItems(count_all_ratings);
+  };
+
+  $scope._createRateItems = function(count_all) {
+    var result = [];
+
+    var i = count_all;
+    var j = 0;
+    while (i--) {
+      result.push( {index : j++, value: 4} )
+    }
+
+    return result;
+  };
+
+  $scope.build_items();
+
+  $scope.checkfields = function () {
+    $scope.incomplete = !$scope.is_valid_fields() || !$scope.model.ratesCount;
+    if (!$scope.incomplete) {
+      $scope.do_calc_parts();
+      $scope.is_accepted_credit = $scope.is_accept();
+    }
+  };
+
+  $scope.is_valid_fields = function () {
+    var is_valid = true;
+
+    for (var i = 0; i < $scope.model.items_rate.length; i++) {
+      var type = $scope.model.items_rate[i].value_type;
+      var value = $scope.model.items_rate[i].value;
+      if (!value && type === 'rating') {
+        is_valid = false;
+      } else {
+        if (type === 'undefined') {
+          is_valid = false;
+        }
+      }
+    }
+    console.log("is_valid." + is_valid);
+    return is_valid;
+  }
+
+
+  $scope.is_accept = function () {
+    return (($scope.calc_middle_mark >= 4.0) && ($scope.calc_disrespectfully < 10.0));
+  };
+
+  $scope.do_calc_parts = function () {
+    console.log("do_calc_parts() ...");
+
+    var
+      count_rates = 0,
+      sum_rates = 0,
+      count_respectfully = 0,
+      count_disrespectfully = 0;
+
+    for (var i = 0; i < $scope.model.items_rate.length; i++) {
+      var type = $scope.model.items_rate[i].value_type;
+      var value = $scope.model.items_rate[i].value;
+      if (value && type === 'rating') {
+        count_rates++;
+        sum_rates = sum_rates + parseInt(value);
+      } else {
+        if (type === 'respectfully') {
+          count_respectfully++;
+        } else if (type === 'disrespectfully') {
+          count_disrespectfully++;
+        }
+      }
+    }
+
+    var count_all_ratings = $scope.model.ratesCount;
+    if (count_all_ratings !== 0) {
+      $scope.calc_respectfully = count_respectfully * 100 / count_all_ratings;
+      $scope.calc_disrespectfully = count_disrespectfully * 100 / count_all_ratings;
+      $scope.calc_middle_mark = count_rates ? sum_rates / count_rates : 0;
+    }
+  };
+
+});
+
+angApp.controller('angController', function ($scope) {
+  $scope.init = function () {
+    conslole.log('init()');
+    $scope.title_subject = "subject_title";
+    $scope.count_ratings = 3;
+    $scope.incomplete = true;
+    $scope.middle_mark = 0.0;
+    $scope.ratesCount = 4;
+  };
+
+});
